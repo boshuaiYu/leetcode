a20ad8ea1c214783fa07c38a6b98eb52d3d36082
boshuaiYu <103256188+boshuaiYu@users.noreply.github.com>
2023-02-02 15:26:29 +0800

    Initial commit

--- 数组/二分法/leetcode704.py
+++ 数组/二分法/leetcode704.py
@@ -1,5 +1,33 @@
 """
 
-    给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target ，
-    写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+    给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
 """
+
+# 解法1：左闭右闭
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        left, right = 0, len(nums)
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid
+
+            else:
+                return mid
+
+        return -1
+
+
+
+
+# leetcode submit region end(Prohibit modification and deletion)
